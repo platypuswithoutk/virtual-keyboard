a8faebb091e9ede3dd865720023a14bde1046c0b
Roksana Dzioba <roksanadzioba@gmail.com>
2020-05-28 14:37:05 +0200

    fix ctrl and alt keys

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+	browser: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+	"linebreak-style": 0
+  },
+};
--- js/main.js
+++ js/main.js
@@ -1,166 +1,135 @@
 window.onload = function virtualKeyboard() {
-    let contentDiv = this.document.createElement('div');
-    contentDiv.className = "main-content";
-    this.document.body.appendChild(contentDiv);
+  const contentDiv = this.document.createElement('div');
+  contentDiv.className = 'main-content';
+  this.document.body.appendChild(contentDiv);
 
-    let desktop = this.document.createElement('div');
-    desktop.className="desktop";
-    contentDiv.appendChild(desktop);
+  const desktop = this.document.createElement('div');
+  desktop.className = 'desktop';
+  contentDiv.appendChild(desktop);
 
-    let resultWindowTextarea = this.document.createElement('textarea');
-    desktop.appendChild(resultWindowTextarea);
+  const resultWindowTextarea = this.document.createElement('textarea');
+  desktop.appendChild(resultWindowTextarea);
 
-    let keyboard = this.document.createElement('div');
-    keyboard.className = "keyboard";
-    contentDiv.appendChild(keyboard);
+  const keyboard = this.document.createElement('div');
+  keyboard.className = 'keyboard';
+  contentDiv.appendChild(keyboard);
 
-    var myArray = [
-        ["`", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", "=", "Backspace"],
-        ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "Delete"],
-        [{name: "Caps Lock", id: 'CapsLock'}, "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter"],
-        [{name: "Shift", id:"ShiftLeft"}, "\\", "Z", "X", "C", "V", "B", "N", "M", ".", ",", "/", {name: "&uarr;", id: 'ArrowUp'}, {name: "Shift", id: "ShiftRight"}],
-        [{name: "Ctrl", id: "ControlLeft"}, "Win", {name: "Alt", id: "AltLeft"}, " ", {name: "Alt", id: "AltRight"}, {name: "Ctrl", id: "ControlRight"}, {name: "&#8592;", id: 'ArrowLeft'}, {name: "&darr;", id: 'ArrowDown'}, {name: "&rarr;", id: 'ArrowRight'}]
-    ]
+  const myArray = [
+    ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', 'Backspace'],
+    ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', { name: 'Del', id: 'Delete' }],
+    [{ name: 'Caps Lock', id: 'CapsLock' }, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter'],
+    [{ name: 'Shift', id: 'ShiftLeft' }, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '.', ',', '/', { name: '&uarr;', id: 'ArrowUp' }, { name: 'Shift', id: 'ShiftRight' }],
+    [{ name: 'Ctrl', id: 'ControlLeft' }, { name: 'Win', id: 'MetaLeft' }, { name: 'Alt', id: 'AltLeft' }, { name: ' ', id: 'Space' }, { name: 'Alt', id: 'AltRight' }, { name: '&#8592;', id: 'ArrowLeft' }, { name: '&darr;', id: 'ArrowDown' }, { name: '&rarr;', id: 'ArrowRight' }, { name: 'Ctrl', id: 'ControlRight' }],
+  ];
 
-    for(let i = 0; i < myArray.length; i++) {
-        let rowDiv = this.document.createElement('div');
-        rowDiv.className = "rowDiv";
-        rowDiv.id = "row" + i;
-        keyboard.appendChild(rowDiv);
-        let rowElements = myArray[i];
-        rowElements.forEach(element => {
-            let rowItem = this.document.createElement('li');
-            if (typeof element == 'string') {
-                rowItem.innerHTML = element;
-                if(element.length > 1) {
-                    rowItem.id = element;
-                } else {
-                    rowItem.id = `Key`+ element.toUpperCase()
-                }
-                
-            } else if (typeof element == 'number') {
-                rowItem.innerHTML = element;
-                rowItem.id = `Digit` + element;
-            } else {
-                rowItem.innerHTML = element.name;
-                rowItem.id = element.id;
-            }
-            rowDiv.appendChild(rowItem);
-        });
-    }
-
-    let specialKeysMap = new Map();
-
-    const onEnter = function onEnter() {
-        resultWindowTextarea.value += '\n' ;
-    }
-    specialKeysMap.set('Enter', onEnter);
-
-    const onTab = function onTab() {
-        resultWindowTextarea.value += '\t' ;
-    }
-    specialKeysMap.set('Tab', onTab);
-
-    const onBack = function onBack() {
-        let initCursorPosition = resultWindowTextarea.selectionStart;
-        let currentText = resultWindowTextarea.value;
-        resultWindowTextarea.value = currentText.slice(0, resultWindowTextarea.selectionStart-1) + currentText.slice(resultWindowTextarea.selectionStart, currentText.length);
-        resultWindowTextarea.selectionStart = initCursorPosition -1;
-        resultWindowTextarea.selectionEnd = initCursorPosition -1;
-    }
-    specialKeysMap.set('Backspace', onBack);
-
-    const leftArrow = function leftArrow() {
-        resultWindowTextarea.selectionEnd -= 1;
-    }
-    specialKeysMap.set('ArrowLeft', leftArrow);
-
-    const rightArrow = function rightArrow() {
-        resultWindowTextarea.selectionStart += 1;
-    }
-    specialKeysMap.set('ArrowRight', rightArrow);
-    
-    document.addEventListener('keydown', (event) => {
-        console.log(event);
-        let pushedKey = event.code;
-        let keyTile = document.getElementById(pushedKey);
-        keyTile.style.backgroundColor = "coral";
-        let text = keyTile.innerText;
-        if (specialKeysMap.has(pushedKey)) {
-            let action = specialKeysMap.get(pushedKey);
-            action();
+  for (let i = 0; i < myArray.length; i += 1) {
+    const rowDiv = this.document.createElement('div');
+    rowDiv.className = 'rowDiv';
+    rowDiv.id = `row${i}`;
+    keyboard.appendChild(rowDiv);
+    const rowElements = myArray[i];
+    rowElements.forEach((element) => {
+      const rowItem = this.document.createElement('li');
+      if (typeof element === 'string') {
+        rowItem.innerHTML = element;
+        if (element.length > 1) {
+          rowItem.id = element;
         } else {
-            resultWindowTextarea.value += text;
+          rowItem.id = `Key${element.toUpperCase()}`;
         }
-       // resultWindowTextarea.focus();
-        event.preventDefault();
-    })
-
-    document.addEventListener('keyup', (event) => {
-        console.log(event);
-        let pushedKey = event.code;
-        let keyTile = document.getElementById(pushedKey);
-        keyTile.style.backgroundColor = "lightgoldenrodyellow";
-        event.preventDefault();
-    })
-
-    
-
-    
-
-    
-
-
-/*
-    let currentItemIndex = 0;
-    let ul = document.createElement('ul');
-    ul.id="menu-list";
-    document.body.appendChild(ul);
-    
-    var names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    let lastItemIndex = names.length - 1;
-    
-    for (var i = 0; i < names.length; i++) {
-        var name = names[i];
-        var li = document.createElement('li');
-        li.id = i;
-        li.innerText = name;
-        ul.appendChild(li);
-    } 
-    
-    document.body.addEventListener('keydown', (event) => {
-        switch (event.key) {
-            case "ArrowRight":
-                ++currentItemIndex;
-                changeNextItemColor();
-                break;
-            case "ArrowLeft":
-                --currentItemIndex;
-                changePreviousItemColor();
-                break;
-        }
+      } else if (typeof element === 'number') {
+        rowItem.innerHTML = element;
+        rowItem.id = `Digit${element}`;
+      } else {
+        rowItem.innerHTML = element.name;
+        rowItem.id = element.id;
+      }
+      rowDiv.appendChild(rowItem);
     });
-    
-    document.getElementById(currentItemIndex).style.backgroundColor = "red";
-    
-    function changeNextItemColor() {
-        let changeIndextoPink = currentItemIndex -1;
-        if (currentItemIndex >= names.length) {
-            currentItemIndex = 0;
-            changeIndextoPink = lastItemIndex; 
-        }
-    
-        document.getElementById(changeIndextoPink).style.backgroundColor = "pink";
-        document.getElementById(currentItemIndex).style.backgroundColor = "red";
+  }
+
+  const specialKeysMap = new Map();
+
+  const onEnter = function onEnter() {
+    resultWindowTextarea.value += '\n';
+  };
+  specialKeysMap.set('Enter', onEnter);
+
+  const onTab = function onTab() {
+    resultWindowTextarea.value += '\t';
+  };
+  specialKeysMap.set('Tab', onTab);
+
+  const onBack = function onBack() {
+    const initCursorPosition = resultWindowTextarea.selectionStart;
+    const currentText = resultWindowTextarea.value;
+    resultWindowTextarea.value = currentText.slice(0, resultWindowTextarea.selectionStart - 1)
+    + currentText.slice(resultWindowTextarea.selectionStart, currentText.length);
+    resultWindowTextarea.selectionStart = initCursorPosition - 1;
+    resultWindowTextarea.selectionEnd = initCursorPosition - 1;
+  };
+  specialKeysMap.set('Backspace', onBack);
+
+  const onDel = function onDel() {
+    const initCursorPosition = resultWindowTextarea.selectionStart;
+    const currentText = resultWindowTextarea.value;
+    resultWindowTextarea.value = currentText.slice(0, resultWindowTextarea.selectionStart)
+    + currentText.slice(resultWindowTextarea.selectionStart + 1, currentText.length);
+    resultWindowTextarea.selectionStart = initCursorPosition;
+    resultWindowTextarea.selectionEnd = initCursorPosition;
+  };
+  specialKeysMap.set('Delete', onDel);
+
+  const leftArrow = function leftArrow() {
+    resultWindowTextarea.selectionEnd -= 1;
+  };
+  specialKeysMap.set('ArrowLeft', leftArrow);
+
+  const rightArrow = function rightArrow() {
+    resultWindowTextarea.selectionStart += 1;
+  };
+  specialKeysMap.set('ArrowRight', rightArrow);
+
+  const CtrLeft = function CtrLeft() {};
+  specialKeysMap.set('ControlLeft', CtrLeft);
+
+  const CtrRight = function CtrRight() {};
+  specialKeysMap.set('ControlRight', CtrRight);
+
+  const AltLeft = function AltLeft() {};
+  specialKeysMap.set('AltLeft', AltLeft);
+
+  const AltRight = function AltRight() {};
+  specialKeysMap.set('AltRight', AltRight);
+
+  const ShiftLeft = function ShiftLeft() {};
+  specialKeysMap.set('ShiftLeft', ShiftLeft);
+
+  const ShiftRight = function ShiftRight() {};
+  specialKeysMap.set('ShiftRight', ShiftRight);
+
+  const Caps = function Caps() {};
+  specialKeysMap.set('CapsLock', Caps);
+
+  document.addEventListener('keydown', (event) => {
+    const pushedKey = event.code;
+    const keyTile = document.getElementById(pushedKey);
+    keyTile.style.backgroundColor = 'coral';
+    const text = keyTile.innerText;
+    if (specialKeysMap.has(pushedKey)) {
+      const action = specialKeysMap.get(pushedKey);
+      action();
+    } else {
+      resultWindowTextarea.value += text;
     }
-    
-    function changePreviousItemColor() {
-        let changeIndextoPink = currentItemIndex + 1; 
-        if (currentItemIndex < 0) {
-            changeIndextoPink = 0;
-            currentItemIndex = lastItemIndex;
-        }
-        document.getElementById(changeIndextoPink).style.backgroundColor = "pink";
-        document.getElementById(currentItemIndex).style.backgroundColor = "red";
-    } */
-}
+    resultWindowTextarea.focus();
+    event.preventDefault();
+  });
+
+  document.addEventListener('keyup', (event) => {
+    const pushedKey = event.code;
+    const keyTile = document.getElementById(pushedKey);
+    keyTile.style.backgroundColor = 'lightgoldenrodyellow';
+    event.preventDefault();
+  });
+};
